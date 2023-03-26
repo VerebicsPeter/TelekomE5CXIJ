c5d86ed62a8a7159ce73a3eb970da58e4f316ae6
PeterPC <peterverebics@gmail.com>
2023-03-26 12:27:17 +0200

    Commiting changes to simulate.py

--- bead_1/simulate.py
+++ bead_1/simulate.py
@@ -27,13 +27,13 @@ demands  = sim_data['simulation']['demands'] # demands
 
 reservations = []                            # no reservations at the start
 
-def get_link(s, e):                         # gets link between two points
+def get_link(s, e):                          # gets link between two points
   for link in links :
     p = link['points']
     if p[0] == s and p[1] == e : return link
   return None
 
-def try_to_reserve(circuit, demand):        # tries to serve a demand between two endpoints
+def res_links(circuit, demand):        # tries to reserve a demand between two endpoints
   exists , path = True , []
   for i in range(len(circuit) - 1) :
     link = get_link(circuit[i], circuit[i+1])
@@ -43,36 +43,40 @@ def try_to_reserve(circuit, demand):        # tries to serve a demand between tw
     for l in path: l['capacity'] -= demand
   return (exists, path)
 
-def free_circuits(t):
-  tofree = list(filter(lambda r: r['end'] == t,reservations))
-  for res in tofree:
-    if res['end'] == t :
-      for link in res['links'] : link['capacity'] += res['demand']
-      reservations.remove(res)
-      print('>>> freed:', res['links'], '\n', reservations, '\n')
-
-for i in range(duration) :
-  print('Time =', i, '\n')
-  
+def res_circuits(t, event_count):
   for d in demands :
-    if d['start-time'] != i : continue
+    if d['start-time'] != t : continue
 
     sp , ep = d['end-points'][0] , d['end-points'][1]
     cs = list(filter(lambda c: c[0] == sp and c[-1] == ep, circuits)) # select possible circuits for d
-    circuit = cs[0]; print(d, " >>> ", circuit)                       # first possible circuit
-    succeeded , reservedLinks = try_to_reserve(circuit, d['demand'])  # values of try_to_reserve
-    print(succeeded, ", reserved: ", reservedLinks, '\n')
+    circuit = cs[0];                                                  # first possible circuit
+    succeeded , reservedLinks = res_links(circuit, d['demand'])       # return values of res_links
     
     if succeeded :
+      event_count += 1; print('{c}. igény foglalás: {sp}<->{ep} st:{t} - sikeres'.format(c = event_count, sp = sp, ep = ep, t = t))
       endtime = d['end-time']
       reservations.append({
         'end': endtime,
         'demand': d['demand'],
         'links': reservedLinks
       })
-      print('reservation succeeded, reservations: ', reservations, '\n')
-  
-  free_circuits(i)
+    else:
+      event_count += 1; print('{c}. igény foglalás: {sp}<->{ep} st:{t} - sikertelen'.format(c = event_count, sp = sp, ep = ep, t = t))  
+  return event_count
 
-# TODO move inner for into own function
-# TODO outputs
+def free_circuits(t, event_count):
+  tofree = list(filter(lambda r: r['end'] == t,reservations))
+  for res in tofree:
+    if res['end'] == t :
+      for link in res['links'] : link['capacity'] += res['demand']
+      reservations.remove(res)
+      sp = res['links'][ 0]['points'][0]
+      ep = res['links'][-1]['points'][1]
+      event_count += 1; print('{c}. igény felszabadítás: {sp}<->{ep} st:{t}'.format(c = event_count, sp = sp, ep = ep, t = t))
+  return event_count
+
+event_count = 0 # purely for printing event numbers to stdout
+
+for i in range(duration):
+  event_count = res_circuits (i, event_count)
+  event_count = free_circuits(i, event_count)
