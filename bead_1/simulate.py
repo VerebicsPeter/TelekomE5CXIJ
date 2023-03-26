77e43dae7132e2e352f8f60dcf9d956dcd301722
PeterPC <peterverebics@gmail.com>
2023-03-26 11:27:27 +0200

    Commiting changes to bead_1

--- bead_1/simulate.py
+++ bead_1/simulate.py
@@ -3,6 +3,8 @@ import json
 
 sim_data = None # dictionary parsed from json
 
+################################################################################################################################
+
 if len(sys.argv) == 2 :
   try:
     with open(sys.argv[1], 'r') as sim_file : sim_data = json.load(sim_file)
@@ -13,23 +15,25 @@ else:
   print('ERROR:\nThis program takes ONE argument (file name)\nNumber of arguments provided: {n}'.format(n = len(sys.argv) - 1))
   exit(1)
 
+################################################################################################################################
+
+duration = sim_data['simulation']['duration']
+
 links    = sim_data['links']                 # links (containing points and capacity)
 
 circuits = sim_data['possible-circuits']     # possible circuits (containing consecutive 'links' [endpoints and switches])
 
-duration = sim_data['simulation']['duration']
-
 demands  = sim_data['simulation']['demands'] # demands
 
-reservations = [] # no reservations at start
+reservations = []                            # no reservations at the start
 
-def get_link(s, e) :
+def get_link(s, e):                         # gets link between two points
   for link in links :
     p = link['points']
     if p[0] == s and p[1] == e : return link
   return None
 
-def try_to_reserve(circuit, demand) :
+def try_to_reserve(circuit, demand):        # tries to serve a demand between two endpoints
   exists , path = True , []
   for i in range(len(circuit) - 1) :
     link = get_link(circuit[i], circuit[i+1])
@@ -39,12 +43,36 @@ def try_to_reserve(circuit, demand) :
     for l in path: l['capacity'] -= demand
   return (exists, path)
 
+def free_circuits(t):
+  tofree = list(filter(lambda r: r['end'] == t,reservations))
+  for res in tofree:
+    if res['end'] == t :
+      for link in res['links'] : link['capacity'] += res['demand']
+      reservations.remove(res)
+      print('>>> freed:', res['links'], '\n', reservations, '\n')
 
-for d in demands :
-  sp = d['end-points'][0]; ep = d['end-points'][1]
-  cs = list(filter(lambda c: c[0] == sp and c[-1] == ep, circuits))
-  circuit = cs[0]
-  print(d, " >>> ", circuit)
-  print(try_to_reserve(circuit, d['demand']))
+for i in range(duration) :
+  print('Time =', i, '\n')
+  
+  for d in demands :
+    if d['start-time'] != i : continue
 
-# TODO free circuits when needed
+    sp , ep = d['end-points'][0] , d['end-points'][1]
+    cs = list(filter(lambda c: c[0] == sp and c[-1] == ep, circuits)) # select possible circuits for d
+    circuit = cs[0]; print(d, " >>> ", circuit)                       # first possible circuit
+    succeeded , reservedLinks = try_to_reserve(circuit, d['demand'])  # values of try_to_reserve
+    print(succeeded, ", reserved: ", reservedLinks, '\n')
+    
+    if succeeded :
+      endtime = d['end-time']
+      reservations.append({
+        'end': endtime,
+        'demand': d['demand'],
+        'links': reservedLinks
+      })
+      print('reservation succeeded, reservations: ', reservations, '\n')
+  
+  free_circuits(i)
+
+# TODO move inner for into own function
+# TODO outputs
